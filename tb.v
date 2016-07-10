b82a9c3c377da06ab7e2bd679a82392157b672e8
yujungchen <steiff.chen@gmail.com>
2016-07-10 22:49:47 +0800

    Add buffer for storing vtx

--- tb.v
+++ tb.v
@@ -18,16 +18,18 @@ module TopModule();
 	wire [31:0] w_DataIn;
 	wire w_Request;
 
-
 	assign w_Address = m_Address;
 	assign w_DataIn = m_Data;
 	assign w_Request = m_Request;
 
-	// Dump waveform
-	initial begin
-		$dumpfile("signal.vcd");
-		$dumpvars;
-	end
+
+	// Test Buffer
+	reg buf_en;
+	reg buf_w;
+	reg [3:0] buf_Addr;
+	reg [31:0] buf_wData;
+	wire [31:0] buf_rData;
+	buffer Exp(.clk(m_clk), .reset(m_rst), .write(buf_w), .en(buf_en), .Addr(buf_Addr), .wData(buf_wData), .rData(buf_rData));
 
 	// Create Clock
 	always begin 
@@ -44,6 +46,9 @@ module TopModule();
 
 	// Fire
 	initial begin
+		$dumpfile("signal.vcd");
+		$dumpvars(0, TopModule);
+
 		m_clk = 1'b0;
 		m_rst = 1'b1;
 		m_Request = 1'b0;
@@ -56,7 +61,23 @@ module TopModule();
 			m_Data = 32'hbf333333;
 			m_rst = 1'b0;
 		#10 m_StartBaryCentric = 1'b1;
-		#200 $finish;
+
+		#15 buf_en = 1'b1;
+			buf_w = 1'b1;
+			buf_Addr = 4'd0;
+			buf_wData = 32'h3f000000;
+		#10 buf_Addr = 4'd1;
+			buf_wData = 32'h3f333333;
+		#10 buf_Addr = 4'd2;
+			buf_wData = 32'hbf000000;
+		#10 buf_w = 1'b0;
+			buf_Addr = 4'd0;
+		#10 buf_Addr = 4'd1;
+		#10 buf_Addr = 4'd2;
+
+
+
+		#1000 $finish;
 	end
 
 endmodule
